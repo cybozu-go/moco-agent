bd214c9c4a168ae74fa1ba78d597b7a0a59f370d
d-kuro <kurosawa7620@gmail.com>
2021-10-27 08:41:33 +0900

    Support MySQL 8.0.27
    Signed-off-by: d-kuro <kurosawa7620@gmail.com>

--- server/mysql_test.go
+++ server/mysql_test.go
@@ -34,7 +34,7 @@ var socketBaseDir = path.Join(os.TempDir(), "moco-agent-test-server")
 
 var MySQLVersion = func() string {
 	if ver := os.Getenv("MYSQL_VERSION"); ver == "" {
-		os.Setenv("MYSQL_VERSION", "8.0.26")
+		os.Setenv("MYSQL_VERSION", "8.0.27")
 	}
 	return os.Getenv("MYSQL_VERSION")
 }()
